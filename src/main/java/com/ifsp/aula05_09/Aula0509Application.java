e52a3520afb96bccc1197c343bb56d8349fa0f00
unknown <matheus.veronez@outlook.com>
2024-09-05 14:36:24 -0300

    Criação do Projeto

---
+++ src/main/java/com/ifsp/aula05_09/Aula0509Application.java
@@ -0,0 +1,13 @@
+package com.ifsp.aula05_09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula0509Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula0509Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifsp/aula05_09/Aula0509ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsp.aula05_09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula0509ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
